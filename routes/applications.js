859940cd53c66a97492bb3fced3924ffd6119dff
Promoworks <35553040+Promoworks@users.noreply.github.com>
2018-03-26 17:53:27 +0530

    UI Designs

--- routes/applications.js
+++ routes/applications.js
@@ -3,20 +3,25 @@ var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-//Website-Designing
-router.get('/websitedesigning',function(req,res){
-   res.render('websitedesigning'); 
+//UI Designs
+router.get('/UIdesigns',function(req,res){
+   res.render('UIdesigns');
 });
+
+//Website-Designing
+// router.get('/websitedesigning',function(req,res){
+//    res.render('websitedesigning');
+// });
 
 //Web-applications
 router.get('/webapplications',function(req,res){
-   res.render('webapplications'); 
+   res.render('webapplications');
 });
 
 
 //Mobile-applications
 router.get('/mobileapplications',function(req,res){
-   res.render('mobileapplications'); 
+   res.render('mobileapplications');
 });
 
 
